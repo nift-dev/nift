7e6237818ff8b60273ce09aa2bf6eb71b6c24189
n-ham <nicholas.charles.ham@gmail.com>
2026-08-26 10:31:36 +1000

    Ownership unit: scope POSIX headers to the non-Windows build
    Move <signal.h> into the existing #ifndef _WIN32 include block (with
    <sys/wait.h> and <unistd.h>); sys/wait.h does not exist under MSYS2 UCRT64.
    The process-death case is already POSIX-only and skipped on Windows.

--- tests/ownership_unit.cpp
+++ tests/ownership_unit.cpp
@@ -8,10 +8,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <filesystem>
-#include <signal.h>
 #include <string>
-#include <sys/wait.h>
-#include <unistd.h>
 
 namespace fs = std::filesystem;
 
@@ -25,6 +22,7 @@ static int failures = 0;
     } while (0)
 
 #ifndef _WIN32
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
